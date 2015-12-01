d07505491c0641e831336a6529e0e4b94c8a0eb1
Kev <wattebled.kev@gmail.com>
2015-12-01 15:22:41 +0100

    gulp OK

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import catatonicCircleKev from '../lib';
 
 describe('catatonic-circle-kev', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+  it('should have a version number!', function () {
+    assert(typeof catatonicCircleKev.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
   });
 });
